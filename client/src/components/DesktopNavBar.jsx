d85c9c4e6abf14c30a8fbc9697333a872069c7ef
Cole Allan <coleallan@Coles-MacBook-Pro.local>
2017-08-01 16:21:27 -0600

    Added to paths in desktopnavbar jsx file on link tags

--- client/src/components/DesktopNavBar.jsx
+++ client/src/components/DesktopNavBar.jsx
@@ -6,25 +6,25 @@ const DesktopNavBar = (props) => {
         <header className="mainHeader">
             <a href="#" /> //This will be the zillow logo on the left of nav bar
             <nav className="desktopNav">
-                <Link className="headerLink">
+                <Link className="headerLink" to='/buy'>
                     <span className="buyLink">Buy</span>
                 </Link>
-                <Link className="headerLink">
+                <Link className="headerLink" to='/rent'>
                     <span className="rentLink">Rent</span>
                 </Link>
-                <Link className="headerLink">
+                <Link className="headerLink" to='/sell'>
                     <span className="sellLink">Sell</span>
                 </Link>
-                <Link className="headerLink">
+                <Link className="headerLink" to='/mortgages'>
                     <span className="mortgagesLink">Mortgages</span>
                 </Link>
-                <Link className="headerLink">
+                <Link className="headerLink" to='/agentfinder'>
                     <span className="agentFinderLink">Agent Finder</span>
                 </Link>
-                <Link className="headerLink">
+                <Link className="headerLink" to='/homedesign'>
                     <span className="homeDesignLink">Home Design</span>
                 </Link>
-                <Link className="headerLink">
+                <Link className="headerLink" to='/more'>
                     <span className="moreLink">More</span>
                 </Link>
             </nav>
