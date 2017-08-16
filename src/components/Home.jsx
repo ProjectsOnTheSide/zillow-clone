8c47ea238b0370431206462c4a7630c532d0035b
Cole Allan <coleallan@Coles-MacBook-Pro.local>
2017-08-16 15:17:02 -0600

    MiddleHomeSection added with text. ready for carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,37 +2,41 @@ import React from 'react';
 // import { Link } from 'react-router';
 import '../styles/main.css';
 import '../styles/home.css';
+import MiddleHomeSection from './MiddleHomeSection.jsx';
 
 const Home = (props) => {
     //Probably need to pass down image from api or amazon on props and set image here
     return (
-        <div className='homeContainer'>
-            <div className='videoSection'>
-                <header className='searchBoxHeader'>
-                    <h1>Find your way home</h1>
-                </header>
-                <section className='searchBox'>
-                    <div className='searchBody'>
-                        <label className='buy'>Buy</label>
-                        <label className='rent'>Rent</label>
-                        <label className='sell'>Sell</label>
-                        <label className='zestimate'>Zestimate</label>
-                    </div>
-                    <div className='searchInputAndButton'>
-                        <input placeholder='Enter an address, neighborhood, city or ZIP code' className='searchInput' type='text' />
-                        <button className='searchButton' type='submit'>
-                            <h5 className='searchBtnText'>Search</h5>
-                        </button>
-                    </div>
-                </section>
-            </div>
-            <div className='editHomeFact'>
-                <div className='updateText'>
-                    <h3 className='homeFacts'>Edit your home facts</h3>
-                    <h4 className='updateDetails'>Update home details faction into your Zestimate.</h4>
+        <div>
+            <div className='homeContainer'>
+                <div className='videoSection'>
+                    <header className='searchBoxHeader'>
+                        <h1>Find your way home</h1>
+                    </header>
+                    <section className='searchBox'>
+                        <div className='searchBody'>
+                            <label className='buy'>Buy</label>
+                            <label className='rent'>Rent</label>
+                            <label className='sell'>Sell</label>
+                            <label className='zestimate'>Zestimate</label>
+                        </div>
+                        <div className='searchInputAndButton'>
+                            <input placeholder='Enter an address, neighborhood, city or ZIP code' className='searchInput' type='text' />
+                            <button className='searchButton' type='submit'>
+                                <h5 className='searchBtnText'>Search</h5>
+                            </button>
+                        </div>
+                    </section>
                 </div>
-                <button className='getStarted'>Get started</button>
+                <div className='editHomeFact'>
+                    <div className='updateText'>
+                        <h3 className='homeFacts'>Edit your home facts</h3>
+                        <h4 className='updateDetails'>Update home details faction into your Zestimate.</h4>
+                    </div>
+                    <button className='getStarted'>Get started</button>
+                </div>  
             </div>
+            <MiddleHomeSection />
         </div>
     )
 }
