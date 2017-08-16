a48267f4355c7dff66bfe92ca624fd43b3be2f5e
Jacob <jacob.lemperle@gmail.com>
2017-08-16 10:33:29 -0600

    carousel showing and working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 // import { Link } from 'react-router';
+import Slider from 'react-slick';
 import '../styles/main.css';
 import '../styles/home.css';
 
 const Home = (props) => {
     //Probably need to pass down image from api or amazon on props and set image here
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      centerMode: true,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      }, {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+    };
     return (
+        <div>
         <div className='homeContainer'>
             <div className='videoSection'>
                 <header className='searchBoxHeader'>
@@ -33,6 +65,20 @@ const Home = (props) => {
                 </div>
                 <button className='getStarted'>Get started</button>
             </div>
+                
+        </div>
+        <div>
+        <Slider 
+                {...settings}
+                >
+                <div><h3>1</h3></div>
+                <div><h3>2</h3></div>
+                <div><h3>3</h3></div>
+                <div><h3>4</h3></div>
+                <div><h3>5</h3></div>
+                <div><h3>6</h3></div>
+                </Slider>
+        </div>
         </div>
     )
 }
